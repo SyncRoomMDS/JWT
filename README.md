6a6526dd712b365189d92850199fd64d58617219
Wordeur <wordeurpbe@gmail.com>
2022-05-03 09:20:58 +0200

    README work

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# JWT
+# SyncRoom - API
+
+## Information
+```
+This project is intended to be used in pair with the Vue App of the same name.
+It runs with Node.js and uses JWT format as well as the ORM Sequelize.
+```
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run start
+```
--- _helpers/db.js
+++ _helpers/db.js
@@ -9,11 +9,11 @@ initialize();
 async function initialize() {
     // Génération de la base de données et / ou connexion
     const { host, port, user, password, database } = config.database;
-    const connection = await mysql.createConnection({ host, port, user, password });
+    const connection = await mysql.createConnection({ host: host, port: port, user: user, password: password });
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // Laison base de données
-    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    const sequelize = new Sequelize(database, user, password, { host: host, port: port, dialect: 'mysql' });
 
     // Initialisation des données
     db.User = require('../users/user.model')(sequelize);
